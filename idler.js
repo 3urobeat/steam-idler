121482a746b32e7780bf38bc67fd65c917db3b5a
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-07-17 13:44:20 +0200

    Added a relog queue to fix cooldown errors when relogging many accounts at once

--- idler.js
+++ idler.js
@@ -5,7 +5,9 @@ const SteamID   = require('steamid');
 
 const config = require("./config.json");
 
-var nextacc = 0
+var nextacc    = 0
+var relogQueue = []; //queue tracking disconnected accounts to relog them after eachother with a delay
+
 
 /* ------------ Functions: ------------ */
 /**
@@ -49,7 +51,7 @@ function importLogininfo(callback) {
  * @param {*} logOnOptions 
  */
 function loginAcc(logOnOptions, index) {
-    let bot = new SteamUser();
+    let bot = new SteamUser({ autoRelogin: false });
 
     console.log(`Logging in ${logOnOptions.accountName}...`)
 
@@ -61,21 +63,62 @@ function loginAcc(logOnOptions, index) {
 
         nextacc = index + 1 //the next index can start
 
+        //If this is a relog then remove this account from the queue and let the next account be able to relog
+        if (relogQueue.includes(index)) {
+            console.log(`[${logOnOptions.accountName}] Relog successful.`)
+
+            relogQueue.splice(relogQueue.indexOf(index), 1) //remove this loginindex from the queue
+        }
+
         if (config.onlinestatus) bot.setPersona(config.onlinestatus) //set online status if enabled (https://github.com/DoctorMcKay/node-steam-user/blob/master/enums/EPersonaState.js)
         bot.gamesPlayed(config.playingGames) //start playing games
+
+        setTimeout(() => {
+            bot.logOff()
+        }, 5000);
     });
 
     bot.on('friendMessage', (steamID, message) => {
         var steamID64 = new SteamID(String(steamID)).getSteamID64()
 
-        console.log(`Friend message from ${steamID64}: ${message}`)
+        console.log(`[${logOnOptions.accountName}] Friend message from ${steamID64}: ${message}`)
 
         if (config.afkMessage.length > 0) {
             console.log("Responding with: " + config.afkMessage)
 
             bot.chat.sendFriendMessage(steamID, config.afkMessage)
-        }
-        
+        }  
+    })
+
+    bot.on("disconnected", (eresult, msg) => { //handle relogging
+        if (relogQueue.includes(index)) return; //don't handle this event if account is already waiting for relog
+
+        console.log(`[${logOnOptions.accountName}] Lost connection to Steam. Message: ${msg}. Trying to relog in ${config.relogDelay / 1000} seconds...`);
+        relogQueue.push(index);
+
+        //Check if it's out turn to relog every 1 sec after waiting relogDelay ms
+        setTimeout(() => {
+            var relogInterval = setInterval(() => {
+                if (relogQueue.indexOf(index) != 0) return; //not our turn? stop and retry in the next iteration
+
+                clearInterval(relogInterval) //prevent any retries
+                bot.logOff()
+
+                console.log(`[${logOnOptions.accountName}] It is now my turn. Relogging in a second...`)
+
+                //Generate steam guard code again if user provided a shared_secret
+                if (logOnOptions["sharedSecretForRelog"]) {
+                    logOnOptions["twoFactorCode"] = SteamTotp.generateAuthCode(logOnOptions["sharedSecretForRelog"]);
+                }
+
+                //Attach relogdelay timeout
+                setTimeout(() => {
+                    console.log(`[${logOnOptions.accountName}] Logging in...`)
+                    
+                    bot.logOn(logOnOptions)
+                }, 1000);
+            }, 1000);
+        }, config.relogDelay);
     })
 
 }
@@ -102,6 +145,7 @@ importLogininfo((logininfo) => {
                     //If a shared secret was provided in the logininfo then add it to logOnOptions object
                     if (e[2] && e[2] != "") { 
                         logOnOptions["twoFactorCode"] = SteamTotp.generateAuthCode(e[2])
+                        logOnOptions["sharedSecretForRelog"] = e[2]; //add raw shared_secret to obj as well to be able to access it from disconnected event
                     }
                 
                     loginAcc(logOnOptions, i)
