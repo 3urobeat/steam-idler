267a6a7cfe71e85bedd8f27ccd41df48d6b6cd8a
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-10-18 11:29:53 +0200

    Added eslint config, enforced styling and fixed a few missing imports

--- idler.js
+++ idler.js
@@ -3,15 +3,15 @@
  * Project: steam-idler
  * Created Date: 31.03.2021 21:05:47
  * Author: 3urobeat
- * 
+ *
  * Last Modified: 17.10.2022 19:10:05
  * Modified By: 3urobeat
- * 
+ *
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 
